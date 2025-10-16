fbcb5bac32fb35d3b08b77b3006492b469e341bc
senorworhley <99400802+senorworhley@users.noreply.github.com>
2025-10-17 00:06:20 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let firstCard, secondCard;
+let sum;
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+firstCard = getRandomInt(1, 11);
+secondCard = getRandomInt(1, 11);
+
+sum = firstCard + secondCard;
+
+console.log("First Card: " + firstCard);
+console.log("Second Card: " + secondCard);
+console.log("Sum: " + sum);
